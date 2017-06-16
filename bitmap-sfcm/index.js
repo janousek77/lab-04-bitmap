5b6c4c7592115a16ad95ce9c7c20dd32a6b1b150
Spencer Gietzen <Speng1516@comcast.net>
2017-06-16 13:53:55 -0700

    invert image done

--- bitmap-sfcm/index.js
+++ bitmap-sfcm/index.js
@@ -6,8 +6,8 @@ let bitmap = require('./lib/bitmap.js');
 let convertImage = (file) => {
   bitmap.readFromFile(file, (data) => {
     let img = new bitmap.Image(data, file);
-    img.invertImg();
+    img.rgbImg();
   });
 };
 
-convertImage('./imgs/3.bmp');
+convertImage('./imgs/2.bmp');
--- bitmap-sfcm/lib/bitmap.js
+++ bitmap-sfcm/lib/bitmap.js
@@ -8,11 +8,11 @@ function Image(buffer, file) {
   this.buffer = buffer;
   this.fileLocation = file;
   this.header = this.buffer.slice(0, 2);
-  this.dib = this.buffer.slice(2, 42);
+  this.dib = this.buffer.slice(2, 54);
   this.height = this.buffer.slice(18, 22).readUInt16LE(0);
   this.width = this.buffer.slice(22, 26).readUInt16LE(0);
-  this.colorTable = this.buffer.slice(42, 1066);
-  this.pixelArr = this.buffer.slice(1066, 1066 + this.width * this.height);
+  this.colorTable = this.buffer.slice(54, 1078);
+  this.pixelArr = this.buffer.slice(1078, 1078 + this.width * this.height);
 }
 
 bitmap.readFromFile = (file, callback) => {
@@ -25,28 +25,27 @@ bitmap.readFromFile = (file, callback) => {
 
 bitmap.Image = Image;
 
-bitmap.Image.prototype.writeToFile = function(file) {
-  console.log('write to file');
+bitmap.Image.prototype.writeToFile = function() {
+  fs.writeFile(this.fileLocation, this.buffer, function(err) {
+    if(err)
+      return console.log(err);
+    console.log('The file was written!');
+  });
 };
 
 bitmap.Image.prototype.invertImg = function() {
-  // console.log(this.buffer.slice(28, 32).readUInt16LE(0));
   for(let i = 0; i < this.colorTable.length; i++) {
     this.colorTable[i] = 255 - this.colorTable[i];
   }
-  // console.log(this.buffer);
-  fs.writeFile(this.fileLocation, this.buffer, function(err) {
-    if(err) {
-      return console.log(err);
-    }
-    console.log('The file was saved!');
-  });
+  this.writeToFile();
 };
 
 bitmap.Image.prototype.grayImg = function() {
 
+  this.writeToFile();
 };
 
 bitmap.Image.prototype.rgbImg = function(color) {
 
+  this.writeToFile();
 };
